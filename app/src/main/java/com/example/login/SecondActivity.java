b09d71d0dfb93b62c0595eba620cd12f40ff135d
Zml99 <malinares99@gmail.com>
2019-05-29 01:25:38 -0400

    Second Activity almost done. Just missing the images.

---
+++ app/src/main/java/com/example/login/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.example.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private ListView lv1;
+
+    private String nombres [] = {"Miguel Ant. Linares S.", "Anthony Hernandez", "Jose Carlos Ramirez", "Eduardo Herrera"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        lv1 = (ListView)findViewById(R.id.lv1);
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_names, nombres);
+        lv1.setAdapter(adapter);
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+}
