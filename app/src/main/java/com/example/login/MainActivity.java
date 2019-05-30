c2dc883e0939e29d5cccafc26441fe8e39bd7098
LiveIVhck <liveivbmx@protonmail.com>
2019-05-30 02:03:58 -0400

    Vamos a ver klk

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,28 +1,17 @@
 package com.example.login;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 4f09425859cf472ba5d5ddadaa940ff1250f94a3
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-<<<<<<< HEAD
-=======
 import android.widget.EditText;
 import android.widget.Toast;
-=======
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
->>>>>>> Log-In Desing
-=======
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
->>>>>>> origin/master
->>>>>>> 4f09425859cf472ba5d5ddadaa940ff1250f94a3
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
+
 
         btn = (Button)findViewById(R.id.btnlog);
 
@@ -43,9 +32,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent3, 0);
             }
         });
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         Button Ingresar = findViewById(R.id.btnlog);
         Ingresar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,10 +48,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-=======
->>>>>>> Log-In Desing
-=======
->>>>>>> origin/master
->>>>>>> 4f09425859cf472ba5d5ddadaa940ff1250f94a3
+
     }
 }
