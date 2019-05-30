4470331bdedc6e0c4c1a8db11b91f1b89f35cceb
LiveIVhck <liveivbmx@protonmail.com>
2019-05-30 00:03:33 -0400

    Prueba de resolucion de los constraints + boton de limpieza (no terminado) On branch JC Changes to be committed: modified:   app/src/main/java/com/example/login/MainActivity.java modified:   app/src/main/res/layout/activity_main.xml modified:   app/src/main/res/layout/activity_second.xml modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
