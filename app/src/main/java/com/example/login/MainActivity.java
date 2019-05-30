9cd98bb24682a5c040ecff843349f567148c3ce2
LiveIVhck <liveivbmx@protonmail.com>
2019-05-30 00:38:03 -0400

    Creo que esta mejor ahora

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.login;
 
+import android.app.admin.DevicePolicyManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
+public Button btnreset;
+public EditText txtuser;
+public EditText editText4;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnreset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtuser.setText("");
+                editText4.setText("");
+            }
+        });
+
 
     }
 
